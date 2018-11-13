c553cc3c4a8b3c656cbfd1c000706edb58647e01
yufrice <c1155208@cstu.nit.ac.jp>
2018-11-14 01:16:01 +0900

    Add vector libs

---
+++ src/Lib/VectorSim.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE ExistentialQuantification #-}
+
+module Lib.VectorSim where
+
+import qualified Data.Vector as V
+
+similarity :: Num a => V.Vector a -> V.Vector a -> a
+similarity = dotProduct
+
+-- O(n) O(n)
+l2Norm :: (Num a, Floating a) => V.Vector a -> a
+l2Norm = sqrt . V.sum . V.map(\x -> x^2)
+
+--  O(n) O(min(mn,n))
+dotProduct :: Num a => V.Vector a -> V.Vector a -> a
+dotProduct = (V.sum .) . V.zipWith (*)
